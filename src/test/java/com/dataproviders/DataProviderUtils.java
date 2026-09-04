a6b2e238b3a68d972ec7998b8933a119cd5807fb
rslohakare <rslohakare1995@gmail.com>
2026-09-04 11:50:59 +0530

    Created the JSON Reader Utility for Reading the Login API test data

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -5,15 +5,16 @@ import java.util.Iterator;
 import org.testng.annotations.DataProvider;
 
 import com.api.pojo.CreateJobPayLoad;
+import com.api.pojo.UserCredentials;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.FakerDataGenerator;
+import com.api.utils.JsonReaderUtil;
 import com.dataproviders.api.bean.UserBean;
 
 public class DataProviderUtils {
 
 	@DataProvider(name = "LoginAPIDataProvider", parallel = true)
 	public static Iterator<UserBean> loginAPIDataProvider() {
-
 		return CSVReaderUtil.loadCSV("testData/LoginCreds.csv", UserBean.class);
 	}
 
@@ -21,9 +22,12 @@ public class DataProviderUtils {
 	public static Iterator<CreateJobPayLoad> createJobFakeDataProvider() {
 		String fakerCount = System.getProperty("fakerCount", "5");
 		int fakerCountInt = Integer.parseInt(fakerCount);
-
 		Iterator<CreateJobPayLoad> payloadIterator = FakerDataGenerator.generateFakeCreateJobData(fakerCountInt);
 		return payloadIterator;
 	}
 
+	@DataProvider(name = "LoginAPIJsonDataProvider", parallel = true)
+	public static Iterator<UserCredentials> loginAPIJsonDataProvider() {
+		return JsonReaderUtil.loadJSON("testData/demo.json", UserCredentials[].class);
+	}
 }
